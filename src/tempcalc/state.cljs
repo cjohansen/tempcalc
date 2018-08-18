1656dce1f5254de71c147c6d00d98642708f355e
Christian Johansen <christian@cjohansen.no>
2018-08-18 23:38:26 +0200

    Keep items a vector when removing items

--- src/tempcalc/state.cljs
+++ src/tempcalc/state.cljs
@@ -17,7 +17,7 @@
   (assoc state :items (conj (vec (calc/solve (:temperature state) (:items state))) {:amount 0})))
 
 (defmethod exec-action :remove-item [_ state idx]
-  (update state :items #(concat (subvec % 0 idx) (subvec % (inc idx)))))
+  (update state :items #(vec (concat (subvec % 0 idx) (subvec % (inc idx))))))
 
 (defn publish [actions & args]
   (reset! store (reduce (fn [state [action & action-args]]
